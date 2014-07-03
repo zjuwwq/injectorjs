c005bfb733a0a005e1c3c7e841a149224de03984
zjuwwq <zjuwwq@163.com>
2014-07-03 08:52:49 +0800

    add grunt config

---
+++ GruntFile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '// <%= pkg.name %>.js v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>) //\n'
+			},
+			build: {
+				src: '<%= pkg.name %>.js',
+				dest: '<%= pkg.name %>.min.js'
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['injector.js'],
+				tasks: ['uglify']
+			}
+		}
+	});
+
+	// Load the plugin that provides tasks.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Default task(s).
+	grunt.registerTask('default', ['uglify', 'watch']);
+
+};
