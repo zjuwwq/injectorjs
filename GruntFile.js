85cb42a3043b67c38717ebd3912fe61545bceb69
zjuwwq <zjuwwq@163.com>
2014-07-03 14:49:40 +0800

    update

--- GruntFile.js
+++ GruntFile.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
 		},
 		watch: {
 			scripts: {
-				files: ['injector.js'],
+				files: ['<%= pkg.name %>.js', 'package.json'],
 				tasks: ['uglify']
 			}
 		}
